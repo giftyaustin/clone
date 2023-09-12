244c2e276656f2fb93d93e5e0de5f270d2b15cbd
giftyaustin <austingifty@gmail.com>
2023-09-13 01:13:29 +0530

    first

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
-import './globals.css'
+import DropDown from '@/components/DropDown'
+
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 
@@ -16,7 +17,19 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>{children}
+      
+      <div className='div'>
+      <DropDown/>
+      <DropDown/>
+      <DropDown/>
+      <DropDown/>
+      </div>
+     
+      
+      
+      
+      </body>
     </html>
   )
 }
---
+++ components/DropDown.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './dropdown.css'
+
+const DropDown = () => {
+  return (
+    <li className='navItem'> News <div>tna</div>
+        <ul className='subMenu'> 
+            <li>wwe</li>
+            <li>wwe</li>
+            <li>wwe</li>
+            <li>wwe</li>
+        </ul>
+    </li>
+  )
+}
+
+export default DropDown
